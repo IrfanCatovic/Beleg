b8513159b6f98c25112e8f0ff0f1c465c5af88e3
Irfan Catovic <56677138+IrfanCatovic@users.noreply.github.com>
2026-02-14 23:29:02 +0100

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,6 +14,7 @@ interface AuthContextType {
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+//i have stoped exploring file here.
 
     export function AuthProvider({ children }: { children: ReactNode }) {
 
