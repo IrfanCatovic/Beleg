6d2fd1a1aaa1f3ba737e66da8ae8d7eb072f29ad
IrfanCatovic <catovicc84@gmail.com>
2026-03-31 18:13:49 +0200

    Enhance internationalization support for ContactPage component
    - Integrated i18next for dynamic text rendering in the ContactPage component, improving user experience with localized messages.
    - Added new translations for contact-related functionalities, including form labels, success messages, and error notifications.
    - Refactored static text to utilize translation functions, ensuring consistency and enhancing accessibility across the application.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -13,7 +13,7 @@ i18n
     fallbackLng: 'sr',
     supportedLngs,
     defaultNS: 'common',
-    ns: ['common', 'landing', 'login', 'appLayout', 'home', 'tasks', 'actions', 'actionDetails', 'users', 'userInfo', 'profileSettings', 'userProfile', 'finance', 'notifications', 'notificationDetails', 'pricing'],
+    ns: ['common', 'landing', 'login', 'appLayout', 'home', 'tasks', 'actions', 'actionDetails', 'users', 'userInfo', 'profileSettings', 'userProfile', 'finance', 'notifications', 'notificationDetails', 'pricing', 'contactPage'],
     interpolation: {
       escapeValue: false,
     },
